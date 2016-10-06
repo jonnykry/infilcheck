4bb2f818e5a806d839c8b34df345691f4d8b0f16
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-10-06 12:08:39 -0500

    template set-up

--- app/server.py
+++ app/server.py
@@ -1,9 +1,35 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
+
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    # DATABASE=os.path.join(app.root_path, 'attendanceapp.db'),
+    DEBUG=True,
+    USERNAME='admin',
+    PASSWORD='admin',
+))
+app.config.from_envvar('ATTENDANCEAPP_SETTINGS', silent=True)
+
+
 
 @app.route('/')
 def hello():
-    return 'Hello world!'
+    return login()
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route("/logout")
+def logout():
+    return redirect('/')
