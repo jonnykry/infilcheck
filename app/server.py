8067de8d6ff4601d708e34fbb32284e97d29a237
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-10-08 15:09:13 -0500

    set up basic pages

--- app/server.py
+++ app/server.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template, redirect
+import boto3
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='templates')
 
+s3 = boto3.resource('s3')
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -14,10 +16,9 @@ app.config.update(dict(
 app.config.from_envvar('ATTENDANCEAPP_SETTINGS', silent=True)
 
 
-
 @app.route('/')
-def hello():
-    return login()
+def home():
+    return dashboard()
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -30,6 +31,16 @@ def register():
     return render_template('register.html')
 
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     return redirect('/')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    return redirect('dashboard')
