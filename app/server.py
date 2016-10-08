10ca1f57c281c872fba1a2735d6c847afa412c0a
bkohl <bkohl@iastate.edu>
2016-10-08 15:12:47 -0500

    login

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect , Response
+from flask.ext.login import LoginManager, UserMixin, login_required
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -15,21 +16,48 @@ app.config.from_envvar('ATTENDANCEAPP_SETTINGS', silent=True)
 
 
 
-@app.route('/')
-def hello():
-    return login()
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
+class User(UserMixin):
+    # proxy for a database of users
+    user_database = {"JohnDoe": ("JohnDoe", "John"),
+                     "JaneDoe": ("JaneDoe", "Jane")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template('register.html')
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username,password = token.split(":") # naive token
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0],user_entry[1])
+            if (user.password == password):
+                return user
+    return None
 
 
-@app.route("/logout")
-def logout():
-    return redirect('/')
+@app.route("/",methods=["GET"])
+def index():
+    return Response(response="Hello World!",status=200)
+
+
+@app.route("/protected/",methods=["GET"])
+@login_required
+def protected():
+    return Response(response="Hello Protected World!", status=200)
+
+
