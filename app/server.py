8b02a3e2ade1b86892276c8645b6e71bac066f48
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-10-08 17:10:58 -0500

    upload to s3 container

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect
 import boto3
+import botocore
 
 app = Flask(__name__, template_folder='templates')
 
@@ -43,11 +44,34 @@ def dashboard():
 
 @app.route('/upload', methods=['POST'])
 def upload_video():
-
     # TODO:  Get userId
-    user_id = 12345
+    user_id = '12345'
+    head_bucket = 'se329-project2'
 
-    s3.create_bucket(Bucket=user_id)
-    
+    bucket = s3.Bucket(head_bucket)
+    exists = True
+
+    try:
+        s3.meta.client.head_bucket(Bucket=user_id)
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+        if error_code == 404:
+            exists = False
+
+    if not exists:
+        s3.create_bucket('se329-project2')
+
+    # TODO: Store the POST data as a temporary `.mov` or `.mp4` file
+
+    filepath = 'app/static/uploads'
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+
+    filepath += '/temp.mp4'
+
+    # TODO:  Uniquely identify each video upload
+    s3.Object(head_bucket, user_id + '/temp.mp4').put(Body=open(filepath, 'rb'))
 
     return redirect('dashboard')
