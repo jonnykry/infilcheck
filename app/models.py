b73949fe43b644503343ff449874e2f6c79cfb1b
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-10-27 17:19:31 -0500

    additional work on database set-up

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
-from app.app import db
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, \
      check_password_hash
 import uuid
 
+
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -41,3 +43,24 @@ class User(db.Model):
     def is_anonymous(self):
         """False, as anonymous users aren't supported."""
         return False
+
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    s3_url = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime)
+
+    def __init__(self, user_id, s3_url, created_at):
+        self.user_id = user_id
+        self.s3_url = s3_url
+        self.created_at = created_at
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_s3_url(self):
+        return self.s3_url
+
+    def get_created_at(self):
+        return self.created_at
