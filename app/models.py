76f2ab5fa2a105c995a0a7f25384dacec3993ce5
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-10-30 14:23:08 -0500

    fix database user table

--- app/models.py
+++ app/models.py
@@ -3,11 +3,10 @@ from werkzeug.security import generate_password_hash, \
      check_password_hash
 import uuid
 
-
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
@@ -47,7 +46,7 @@ class User(db.Model):
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     s3_url = db.Column(db.String(120))
     created_at = db.Column(db.DateTime)
 
