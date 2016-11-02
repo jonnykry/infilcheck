d3f11b8c0a31607e123e90a1f2b888a13746acff
jonnykry93@gmail.com <jonnykry93@gmail.com>
2016-11-01 20:33:25 -0500

    fix user bug

--- app/app.py
+++ app/app.py
@@ -320,7 +320,7 @@ def poll():
                 'output_framerate': pi_obj.output_framerate,
                 'is_enabled': pi_obj.is_enabled
             }
-            flag=Flags.query.filter_by(id=flask_login.current_user.id).first()
+            flag=Flags.query.filter_by(id = user.id).first()
             flag.request_update_settings = False
             db.session.commit()
 
